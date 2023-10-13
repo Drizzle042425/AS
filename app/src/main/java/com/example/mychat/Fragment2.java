180d246d92fd1c4023f8523d7afb3af29eb0441c
Drizzle042425 <3033113256@qq.com>
2023-10-13 08:56:37 +0800

    微信

--- app/src/main/java/com/example/mychat/Fragment2.java
+++ app/src/main/java/com/example/mychat/Fragment2.java
@@ -1,19 +1,60 @@
 package com.example.mychat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class Fragment2 extends Fragment {
 
+public class Fragment2 extends Fragment{   //继承自Fragment类的weixinFragment类。在该类中，重写了onCreateView方法用于创建视图
+    private RecyclerView recyclerView;
+    private List<String> list;
+    private Context context;
+    private Myadapter myadapter;
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab02, container, false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.tab02,container,false);
+        context=view.getContext();
+        recyclerView=view.findViewById(R.id.recycleview);
+        list=new ArrayList();
+
+        initial();
+        myadapter = new Myadapter(context,list);
+        recyclerView.setAdapter(myadapter);
+        LinearLayoutManager manager=new LinearLayoutManager(context);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+
+        return view;
+
     }
+
+    private void initial() {
+        list.add("蔡徐坤");
+        list.add("陈立农");
+        list.add("范丞丞");
+        list.add("黄明昊");
+        list.add("林彦俊");
+        list.add("朱正廷");
+        list.add("王子异");
+        list.add("王琳凯");
+        list.add("尤长靖");
+        list.add("nine percent(＾－＾)V");
+    }
+
 }
+
